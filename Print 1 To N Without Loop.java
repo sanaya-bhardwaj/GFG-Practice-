74b2a23cfe98cfc2b73124c07679702234dff72b
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2024-12-18 16:11:58 +0530

    Create Print 1 To N Without Loop.java

---
+++ Print 1 To N Without Loop.java
@@ -0,0 +1,16 @@
+//Print numbers from 1 to n without the help of loops. 
+//You only need to complete the function printNos() that takes n as a parameter and prints the number from 1 to n recursively.
+//Note: Don't print any newline, it will be added by the driver code.
+
+class Solution {
+    public static void printNos(int n) {
+        // Base condition: If n reaches 0, end recursion
+        if (n == 0) {
+            return;
+        }
+        // Recursive call to handle smaller problem (printing 1 to n-1)
+        printNos(n - 1);
+        // Print the current number after smaller problems are solved
+        System.out.print(n);
+    }
+}
