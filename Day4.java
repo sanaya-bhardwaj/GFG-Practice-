515a96b434db07a09e78e7ebc1c09aa72eb5e664
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-03-29 19:37:44 +0530

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n = arr.length;
+        d%=n;
+        int [] temp = new int[n];
+        for(int i=0;i<n-d;i++)
+        temp[i]=arr[d+i];
+        for(int i=0;i<d;i++)
+        temp[n-d+i]=arr[i];
+        for(int i=0;i<n;i++)
+        arr[i]=temp[i];
+    }
+}
