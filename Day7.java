9ee3bd2a150c7f184adf28e4940be1b992e69ace
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-04-01 20:55:46 +0530

    Update Day7.java

--- Day7.java
+++ Day7.java
@@ -1,1 +1,24 @@
-day 7
+
+
+// User function Template for Java
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // code here
+        
+        int n = prices.length;
+        
+        int ans = 0;
+        
+        int start = prices[0];
+        for(int i=0;i<n-1;i++){
+            
+            if(prices[i] > prices[i+1]){
+                ans += prices[i]-start;
+                start = prices[i+1];
+            }
+        }
+        ans += prices[n-1]-start;
+        
+        return ans;
+    }
+}
