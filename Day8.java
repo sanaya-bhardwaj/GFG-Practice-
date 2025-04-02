ef271a16bfbd692605fc98fd3806274958386be7
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-04-02 20:25:25 +0530

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -1,1 +1,33 @@
 
+
+// User function Template for Java
+
+class Solution {
+    public int maximumProfit(int prices[]) {
+        
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        int res=0;
+  /*  for(int i=0;i<prices.length;i++)
+       {
+           for(int j=i+1;j<prices.length;j++)
+           {
+              
+                res=-1*(prices[i]-prices[j]);
+               if(res>max) max=res;
+           }
+       }
+       if(max<0) return 0;
+       return max;
+     */
+        
+        for(int price:prices)
+        {
+            min=Math.min(price,min);
+            res=price-min;
+            max=Math.max(res,max);
+        }
+        return max;
+    
+    }
+}
