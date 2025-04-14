eed3422cfa274effdce047ec2958d93cf9adfc1c
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-04-14 22:17:12 +0530

    Update Day20.java

--- Day20.java
+++ Day20.java
@@ -1,80 +1,57 @@
+
 class Solution {
-
-    public boolean KMPSearch(String pat, String txt) {
-        int M = pat.length();
-        int N = txt.length();
-
-        // create lps[] that will hold the longest prefix suffix
-        // values for pattern
-        int[] lps = new int[M];
-
-        // Preprocess the pattern (calculate lps[] array)
-        computeLPSArray(pat, M, lps);
-
-        int i = 0; // index for txt[]
-        int j = 0; // index for pat[]
-        while ((N - i) >= (M - j)) {
-            if (pat.charAt(j) == txt.charAt(i)) {
-                j++;
-                i++;
-            }
-
-            if (j == M) {
-                return true;
-            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
-                // mismatch after j matches
-                // Do not match lps[0..lps[j-1]] characters,
-                // they will match anyway
-                if (j != 0)
-                    j = lps[j - 1];
-                else
-                    i = i + 1;
-            }
+    // Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2) {
+        // Your code here
+        if (s1.length() != s2.length()) {
+            return false;
         }
-        return false;
+        String str = s1 + s1;
+        return search(str, s2);
     }
-
-    // Fills lps[] for given pattern pat[0..M-1]
-    void computeLPSArray(String pat, int M, int[] lps) {
-        // length of the previous longest prefix suffix
-        int len = 0;
-
-        lps[0] = 0; // lps[0] is always 0
-
-        // the loop calculates lps[i] for i = 1 to M-1
-        int i = 1;
-        while (i < M) {
-            if (pat.charAt(i) == pat.charAt(len)) {
-                len++;
-                lps[i] = len;
+    
+    private static boolean search(String text, String pattern){
+        int []lps = computeLPS(pattern);
+        int i = 0, j = 0;
+        while (i < text.length()) {
+            if (pattern.charAt(j) == text.charAt(i)) {
                 i++;
-            } else { // (pat.charAt(i) != pat.charAt(len))
-                if (len != 0) {
-                    len = lps[len - 1];
-
-                    // Also, note that we do not increment
-                    // i here
-                } else { // if (len == 0)
-                    lps[i] = 0;
+                j++;
+            }
+            if (j == pattern.length()) {
+                return true; // Match found
+            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
                     i++;
                 }
             }
         }
+        return false; // No match found
     }
-
-    // Function to check if two strings are rotations of each other or not.
-    public boolean areRotations(String s1, String s2) {
-        if (s1.length() != s2.length())
-            return false;
-        else {
-            // we concatenate first string to itself and check if other
-            // string occurs in it as substring. If yes, then it
-            // is rotated version and we return true else false.
-            String concatenated = s1 + s1;
-            if (KMPSearch(s2, concatenated))
-                return true;
-            else
-                return false;
+    
+    private static int []computeLPS(String pattern){
+        int m = pattern.length();
+        int []lps = new int[m]; // longest prefix suffix
+        int len = 0;
+        lps[0] = 0; // first element has no similar prefix or suffix
+        int i = 1;
+        
+        while(i<m){
+            if(pattern.charAt(i) == pattern.charAt(len)){
+                len++; // increase length of matching prefix suffix
+                lps[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lps[len - 1]; // use previous LPS to check for shorter prefix
+                }else{
+                    lps[i] = 0; // no match so LPS value to 0
+                    i++; // move to next character
+                }
+            }
         }
+        return lps;
     }
 }
