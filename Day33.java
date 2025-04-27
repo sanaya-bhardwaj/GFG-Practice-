52498ecac9e88c6b23de161b69a5573b68e38bd2
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-04-27 19:55:20 +0530

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,49 @@
+// Solution class for solving the problem
+class Solution {
+    // function to check if we can place k cows
+    // with at least dist distance apart
+    static boolean check(int[] stalls, int k, int dist) {
+
+        // Place first cow at 0th index
+        int cnt = 1;
+        int prev = stalls[0];
+        for (int i = 1; i < stalls.length; i++) {
+
+            // If the current stall is at least dist away
+            // from the previous one place the cow here
+            if (stalls[i] - prev >= dist) {
+                prev = stalls[i];
+                cnt++;
+            }
+        }
+
+        // Return true if we are able to place all 'k' cows
+        return (cnt >= k);
+    }
+
+    static int aggressiveCows(int[] stalls, int k) {
+
+        // sorting the array to ensure stalls in sequence
+        Arrays.sort(stalls);
+        int res = 0;
+
+        // Search Space for Binary Search
+        int lo = 1;
+        int hi = stalls[stalls.length - 1] - stalls[0];
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            // If the mid distance is possible, update
+            // the result and search for larger distance
+            if (check(stalls, k, mid)) {
+                res = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return res;
+    }
+}
