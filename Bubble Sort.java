7411c47ce23a867b88c60e917848edab8e0714e6
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-08-23 22:54:46 +0530

    Create Bubble Sort.java

---
+++ Bubble Sort.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
