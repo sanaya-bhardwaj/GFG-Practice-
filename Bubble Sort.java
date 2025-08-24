7d8519f4a2522e5d93f0c684408296de9c648e47
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-08-24 22:13:38 +0530

    Update Bubble Sort.java

--- Bubble Sort.java
+++ Bubble Sort.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = n - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
+                    int temp = arr[j];    //swap
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
