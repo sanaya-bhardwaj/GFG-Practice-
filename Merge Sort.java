3ca5b81e61fea734f68b0cfe8949a577ebebe580
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-08-25 16:29:25 +0530

    Update Merge Sort.java

--- Merge Sort.java
+++ Merge Sort.java
@@ -1,1 +1,27 @@
+class Solution {
+    void mergeSort(int arr[], int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
 
+    void merge(int arr[], int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= high) temp[k++] = arr[j++];
+
+        for (i = 0; i < temp.length; i++) arr[low + i] = temp[i];
+    }
+}
