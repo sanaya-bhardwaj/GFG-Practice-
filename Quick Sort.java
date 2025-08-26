cab87a47985ee09117cd9a87efd688023e444d34
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-08-26 21:11:15 +0530

    Create Quick Sort.java

---
+++ Quick Sort.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void quickSort(int[] arr, int low, int high) {
+        // code here
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        // code here
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, low, j); // put pivot in correct place
+        return j;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
