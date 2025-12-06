cf78bab7f777e11ee40dba2273528f119abfa930
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-12-06 16:37:52 +0530

    Create Check if array is sorted.java

---
+++ Check if array is sorted.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[i-1]){
+                
+            }else{
+                return false;
+            }
+        }return true;
+    }
+}
